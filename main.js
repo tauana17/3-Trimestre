e4f56ca95814bb45301027c94177e9811cfdb2e1
tauana17 <104360790+tauana17@users.noreply.github.com>
2023-11-01 13:50:39 -0300

    01/11

--- main.js
+++ main.js
@@ -2,6 +2,14 @@
 function tocaSomPom(){
 document.querySelector('#som_tecla_pom').play();
 }
-
-
 const listadeteclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador <listadeteclas.length){
+
+    listadeteclas[0] .onclick = tocaSomPom;
+    contador = contador + 1;
+
+    console.log(contador);
+}
