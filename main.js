4bb62d042599b68eb792d91b76076e56d7984d18
tauana17 <104360790+tauana17@users.noreply.github.com>
2023-11-22 14:10:39 -0300

    22/23

--- main.js
+++ main.js
@@ -7,9 +7,14 @@ const listadeteclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while(contador <listadeteclas.length){
+    const tecla = listadeteclas[contador];
+    const instrumentos = tecla.classlist[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    console.log(idAudio)
 
-    listadeteclas[contador] .onclick = function(){
-        tocaSom('#som_tecla_splash')
+    tecla.onclick = function () {
+        tocaSom(idAudio)
     };
     contador = contador + 1;
 
